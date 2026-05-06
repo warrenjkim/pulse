0273d949dc2b42793fa4e62606e5fb159e923181
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-05 21:32:20 -0700

    fix macro

--- dsa/result.h
+++ dsa/result.h
@@ -41,4 +41,11 @@ class [[nodiscard]] Result {
     }                                    \
   } while (false)
 
+#define ASSIGN_OR_RETURN(var, expr)    \
+  auto&& _result = (expr);             \
+  if (!_result.ok()) {                 \
+    return std::move(_result).error(); \
+  }                                    \
+  var = *std::move(_result);
+
 }  // namespace pulse
