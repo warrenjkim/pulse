cb961c066aa50147db2cddcc157a898a6b1f166f
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-05 21:11:01 -0700

    Result (#14)
    * wrapper around std::variant

---
+++ dsa/result.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <variant>
+
+#include "dsa/error.h"
+
+namespace pulse {
+
+template <typename T>
+class [[nodiscard]] Result {
+ public:
+  Result(T value) : data_(std::move(value)) {}
+
+  Result(Error error) : data_(std::move(error)) {}
+
+  bool ok() const { return std::holds_alternative<T>(data_); }
+
+  const Error& error() const& { return std::get<Error>(data_); }
+
+  Error&& error() && { return std::get<Error>(std::move(data_)); }
+
+  const T& operator*() const& { return std::get<T>(data_); }
+
+  T& operator*() & { return std::get<T>(data_); }
+
+  T&& operator*() && { return std::get<T>(std::move(data_)); }
+
+  const T* operator->() const { return &std::get<T>(data_); }
+
+  T* operator->() { return &std::get<T>(data_); }
+
+ private:
+  std::variant<T, Error> data_;
+};
+
+}  // namespace pulse
