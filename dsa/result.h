5ba322e01c73017ac060986d81d3f51237b82187
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-09 02:25:23 -0700

    specialization for Result<void>

--- dsa/result.h
+++ dsa/result.h
@@ -6,7 +6,6 @@
 
 namespace pulse {
 
-// TODO(template specialization for Result<void>)
 template <typename T>
 class [[nodiscard]] Result {
  public:
@@ -32,6 +31,25 @@ class [[nodiscard]] Result {
 
  private:
   std::variant<T, Error> data_;
+};
+
+template <>
+class [[nodiscard]] Result<void> {
+ public:
+  Result() : data_(OkTag{}) {}
+
+  Result(Error error) : data_(std::move(error)) {}
+
+  bool ok() const { return std::holds_alternative<OkTag>(data_); }
+
+  const Error& error() const& { return std::get<Error>(data_); }
+
+  Error&& error() && { return std::get<Error>(std::move(data_)); }
+
+ private:
+  struct OkTag {};
+
+  std::variant<OkTag, Error> data_;
 };
 
 }  // namespace pulse
