31388ecff6cb6f9344993c5ed759cfc47f7c4869
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-10 23:18:54 -0700

    to_string Method

--- http/method.h
+++ http/method.h
@@ -1,7 +1,29 @@
 #pragma once
 
+#include <ostream>
+#include <string>
+
 namespace pulse::http {
 
 enum class Method { kGet, kPost, kPut, kDelete };
+
+inline std::string to_string(Method method) {
+  switch (method) {
+    case Method::kGet:
+      return "GET";
+    case Method::kPost:
+      return "POST";
+    case Method::kPut:
+      return "PUT";
+    case Method::kDelete:
+      return "DELETE";
+    default:
+      return "UNKNOWN";
+  }
+}
+
+inline std::ostream& operator<<(std::ostream& os, Method method) {
+  return os << to_string(method);
+}
 
 }  // namespace pulse::http
