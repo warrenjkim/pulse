9e15452f4d1e29fa41db8841c5aaeb643fc97ef1
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-10 22:40:19 -0700

    fixes

--- http/server.cc
+++ http/server.cc
@@ -1,5 +1,6 @@
 #include "http/server.h"
 
+#include <iostream>
 #include <map>
 #include <memory>
 #include <unordered_map>
@@ -24,47 +25,51 @@ void Server::route(Method method, std::string path,
 
 void Server::run() {
   while (true) {
-    net::Socket socket = acceptor_.accept();
-    if (!socket.ok()) {
+    net::Socket s = acceptor_.accept();
+    if (!s.ok()) {
       continue;
     }
 
-    pool_.submit([this, &socket] {
-      std::string header = socket.read_until("\r\n\r\n");
+    // TODO(use move-only functions)
+    pool_.submit([this, socket = std::make_shared<net::Socket>(std::move(s))] {
+      std::string header = socket->read_until("\r\n\r\n");
+      std::cerr << "header: [" << header << "]\n";
       if (header.empty()) {
+        std::cerr << "empty header\n";
         return;
       }
 
       Result<Request> request = parse_header(header);
       if (!request.ok()) {
-        socket.write(to_string(Response{.content_type = "text/html",
-                                        .status = 400,
-                                        .body = "<h1>400 Bad Request</h1>"}));
+        std::cerr << "parse error: " << request.error().message << "\n";
+        socket->write(to_string(Response{.content_type = "text/html",
+                                         .status = 400,
+                                         .body = "<h1>400 Bad Request</h1>"}));
         return;
       }
 
       auto method = router_.find(request->method);
       if (method == router_.end()) {
-        socket.write(to_string(Response{.content_type = "text/html",
-                                        .status = 404,
-                                        .body = "<h1>404 Not Found</h1>"}));
+        socket->write(to_string(Response{.content_type = "text/html",
+                                         .status = 404,
+                                         .body = "<h1>404 Not Found</h1>"}));
         return;
       }
 
       auto handler = method->second.find(request->path);
       if (handler == method->second.end()) {
-        socket.write(to_string(Response{.content_type = "text/html",
-                                        .status = 404,
-                                        .body = "<h1>404 Not Found</h1>"}));
+        socket->write(to_string(Response{.content_type = "text/html",
+                                         .status = 404,
+                                         .body = "<h1>404 Not Found</h1>"}));
         return;
       }
 
       if (auto it = request->headers.find("Content-Length");
           it != request->headers.end()) {
-        request->body = socket.read(std::stoul(it->second));
+        request->body = socket->read(std::stoul(it->second));
       }
 
-      socket.write(to_string((*handler->second)(*request)));
+      socket->write(to_string((*handler->second)(*request)));
     });
   }
 }
