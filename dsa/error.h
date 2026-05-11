83d059a902dc99c3634e27a13728b715040dbedd
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-10 23:17:37 -0700

    to_string for Error, Result

--- dsa/error.h
+++ dsa/error.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ostream>
 #include <string>
 
 namespace pulse {
@@ -10,5 +11,25 @@ struct Error {
   Code code;
   std::string message;
 };
+
+inline std::string to_string(const Error::Code& code) {
+  switch (code) {
+    case Error::Code::kInternal:
+    default:
+      return "kInternal";
+  }
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Error::Code& code) {
+  return os << to_string(code);
+}
+
+inline std::string to_string(const Error& error) {
+  return "[" + to_string(error.code) + "] " + error.message;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Error& error) {
+  return os << to_string(error);
+}
 
 }  // namespace pulse
--- dsa/result.h
+++ dsa/result.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+#include <sstream>
 #include <variant>
 
 #include "dsa/error.h"
@@ -51,5 +53,31 @@ class [[nodiscard]] Result<void> {
  private:
   std::variant<Ok, Error> data_;
 };
+
+template <typename T>
+  requires requires(std::ostream& os, const T& t) { os << t; }
+inline std::string to_string(const Result<T>& result) {
+  if (result.ok()) {
+    std::ostringstream oss;
+    oss << "Ok(" << *result << ")";
+    return oss.str();
+  }
+
+  return "Err(" + to_string(result.error()) + ")";
+}
+
+template <typename T>
+  requires requires(std::ostream& os, const T& t) { os << t; }
+inline std::ostream& operator<<(std::ostream& os, const Result<T>& result) {
+  return os << to_string(result);
+}
+
+inline std::string to_string(const Result<void>& result) {
+  return result.ok() ? "Ok" : "Err(" + to_string(result.error()) + ")";
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Result<void>& result) {
+  return os << to_string(result);
+}
 
 }  // namespace pulse
