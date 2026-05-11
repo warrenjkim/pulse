02181c61ddd36a545adb432372ba8b0f512af08f
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-10 23:28:13 -0700

    to_string for Request, Response

--- http/request.h
+++ http/request.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ostream>
 #include <string>
 #include <unordered_map>
 
@@ -17,5 +18,33 @@ struct Request {
 
   friend bool operator==(const Request&, const Request&) = default;
 };
+
+inline std::string to_string(const Request& req) {
+  std::string out = "Request{\n  method: " + to_string(req.method) +
+                    "\n  path: " + req.path + "\n";
+  if (!req.params.empty()) {
+    out += "  params: {\n";
+    for (const auto& [k, v] : req.params) {
+      out += "    " + k + ": " + v + "\n";
+    }
+
+    out += "  }\n";
+  }
+
+  if (!req.headers.empty()) {
+    out += "  headers: {\n";
+    for (const auto& [k, v] : req.headers) {
+      out += "    " + k + ": " + v + "\n";
+    }
+
+    out += "  }\n";
+  }
+
+  return out + (req.body.empty() ? "" : "  body: " + req.body) + "\n}";
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Request& req) {
+  return os << to_string(req);
+}
 
 }  // namespace pulse::http
