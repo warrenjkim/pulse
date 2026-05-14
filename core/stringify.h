9acfc0e3fcbd544c7d483de64e002e02322e46d9
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-13 22:33:21 -0700

    stringify struct

---
+++ core/stringify.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+namespace pulse {
+
+template <typename T>
+struct Stringify {
+  static std::string to_string(const T& value);
+};
+
+template <typename T>
+concept Stringifiable = requires(const T& t) {
+  { Stringify<T>::to_string(t) } -> std::convertible_to<std::string>;
+};
+
+template <Stringifiable T>
+std::string to_string(const T& value) {
+  return Stringify<T>::to_string(value);
+}
+
+}  // namespace pulse
