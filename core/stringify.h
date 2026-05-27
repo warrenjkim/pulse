7bcd9cb51a3ea2e0127a36253377517199e978d6
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-26 22:37:04 -0700

    Fix

--- core/stringify.h
+++ core/stringify.h
@@ -20,4 +20,14 @@ std::string to_string(const T& value) {
   return Stringify<T>::to_string(value);
 }
 
+template <typename T>
+concept StdToStringable = requires(const T& t) {
+  { std::to_string(t) } -> std::same_as<std::string>;
+};
+
+template <StdToStringable T>
+struct Stringify<T> {
+  static std::string to_string(const T& value) { return std::to_string(value); }
+};
+
 }  // namespace pulse
